7a7506d318237ec969de9f3f8e47e4cdc8d28c78
Dmitry Fokin <dkoder.dvl@gmail.com>
2022-02-19 09:55:38 +0300

    update

---
+++ application/schemas/crud/Person.js
@@ -0,0 +1,8 @@
+({
+  Entity: {},
+
+  login: { type: 'string', length: { min: 8, max: 64 }, unique: true },
+  password: { type: 'string', note: 'Password hash' },
+  roles: { many: 'Role' },
+  name: {type: 'string'},
+});
